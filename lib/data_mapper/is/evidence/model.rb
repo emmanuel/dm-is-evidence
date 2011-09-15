3fa2e5fa662312187874e38dfaa894dda4227f8d
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2011-09-15 13:47:45 -0700

    Only generate Action subclasses for audited resources.

--- lib/data_mapper/is/evidence/model.rb
+++ lib/data_mapper/is/evidence/model.rb
@@ -7,21 +7,8 @@ module DataMapper::Is::Evidence
       audited       = options.fetch(:audited, false)
       audit_options = audited || {}
 
-      if !defined?(self::Action) and action_base_model = audit_options.fetch(:via, nil)
-        # const_set :Action, Class.new(action_base_model)
-        # self::Action.is :an_action, on: self
-        # ^ this fails because DescendantSet is indexed by name, and the new
-        # dynamic subclass inherits from action_base_model before the constant
-        # is set, and therefore before the new model has a non-nil model.name.
-        self.module_eval <<-RUBY, __FILE__, __LINE__ + 1
-          class Action < #{action_base_model.name}
-            is :an_action, :on => #{self.name}
-          end
-        RUBY
-      end
-
-      unless self < DataMapper::Is::Evidence::Versioned::Resource
-        @versioned_on ||= DataMapper::Is::Evidence::Model.filter_properties(properties, options)
+      unless self < Versioned::Resource
+        @versioned_on ||= Model.filter_properties(properties, options)
 
         # TODO: create version model if not already defined. something like:
         #   if !defined?(self::Version)
@@ -30,12 +17,25 @@ module DataMapper::Is::Evidence
         #   end
         #   @version_model = self::Version
 
-        include DataMapper::Is::Evidence::Versioned::Resource
+        include Versioned::Resource
       end
 
-      if audited and !(self < DataMapper::Is::Evidence::Audited::Resource)
-        @audited_on  ||= DataMapper::Is::Evidence::Model.filter_properties(properties, audit_options)
+      if audited and !(self < Audited::Resource)
+        @audited_on  ||= Model.filter_properties(properties, audit_options)
         @actor_model ||= audit_options.fetch(:actor) { DataMapper::Is::Evidence.actor_model }
+        action_base_model = audit_options.fetch(:via, nil)
+
+        if !defined?(self::Action) and action_base_model
+          self.module_eval <<-RUBY, __FILE__, __LINE__ + 1
+            class Action < #{action_base_model.name}
+              is :an_action, :on => #{self.name}
+            end
+          RUBY
+        end
+
+        include Audited::Resource
+      end
+    end
 
 
     # call VersionedResource::Version.is :a_version, :of => VersionedResource
@@ -59,12 +59,12 @@ module DataMapper::Is::Evidence
 
 
     # call ActorModel.is :audited_actor
-        include DataMapper::Is::Evidence::Audited::Resource
-      end
-    end
-
     def is_audited_actor(options = {})
       @action_model = options.fetch(:action) { raise ArgumentError, "expected :action option" }
+
+      include Audited::Actor
+    end
+
 
     # call ActorModel::Action.is :an_action
     def is_an_action(options = {})
@@ -78,10 +78,6 @@ module DataMapper::Is::Evidence
 
         include Audited::AuditedAction
       end
-    end
-
-
-      include DataMapper::Is::Evidence::Audited::Actor
     end
 
     def self.filter_properties(properties, options)
