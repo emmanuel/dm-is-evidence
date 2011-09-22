f63362411f3786d291c1d515fbfc83c7a72762cf
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2011-09-22 13:55:09 -0700

    Extract before/after callback logic into methods in Versioned::Resource.

--- lib/data_mapper/model/is/evidence/versioned/resource.rb
+++ lib/data_mapper/model/is/evidence/versioned/resource.rb
@@ -1,36 +1,40 @@
 module DataMapper::Model::Is::Evidence
   module Versioned
     module Resource
-      def self.included(model)
-        model.extend ClassMethods
+      def self.included(versioned_model)
+        versioned_model.class_eval do
+          extend ClassMethods
 
-        # soft-delete versioned resources to preserve referential integrity
-        model.property :deleted_at, DataMapper::Property::ParanoidDateTime
+          # soft-delete versioned resources to preserve referential integrity
+          #   with versions stored in related table
+          property :deleted_at, DataMapper::Property::ParanoidDateTime
 
-        model.before :save do
-          @_previous_original_attributes = original_attributes.dup.freeze
+          before :save,   :capture_original_attributes
+          after  :create, :record_create_event
+          after  :update, :record_update_event
         end
+      end
 
-        model.after :create do
-          model::Version.record_event(self, :create) if clean? && saved?
+      # @api private
+      def capture_original_attributes
+        @_previous_original_attributes = original_attributes.dup.freeze
+      end
+
+      # @api private
+      def record_create_event
+        model.version_model.record_event(self, :create) if clean? && saved?
+      end
+
+      # @api private
+      def record_update_event
+        if clean? && _previous_original_attributes.any?
+          # deleted_at is set on destroy by ParanoidDateTime (instead of destroying)
+          event = deleted_at.nil? ? :update : :destroy
+          # ParanoidDateTime sets persisted_state to Immutable,
+          # so don't try to clear @_previous_original_attributes
+          model.version_model.record_event(self, event)
+          @_previous_original_attributes = {} if deleted_at.nil?
         end
-
-        model.after :update do
-          if clean? && _previous_original_attributes.any?
-            # deleted_at is set on destroy by ParanoidDateTime (instead of destroying)
-            event = deleted_at.nil? ? :update : :destroy
-            # ParanoidDateTime sets persisted_state to Immutable,
-            # so don't try to clear @_previous_original_attributes
-            model::Version.record_event(self, event)
-            @_previous_original_attributes = {} if deleted_at.nil?
-          end
-        end
-
-        # ParanoidDateTime causes #destroy calls to become updates,
-        # rendering this superfluous:
-        # model.after :destroy do
-        #   model::Version.record_event(self, :destroy) if clean?
-        # end
       end
 
       # @api private
